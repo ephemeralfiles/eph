51a1f9933dd99c85dcc1de78b43d76d3c71df7b2
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2025-02-24 22:03:45 +0100

    refactor: simplify upload function and remove unused code

--- pkg/ephcli/upload.go
+++ pkg/ephcli/upload.go
@@ -1,7 +1,6 @@
 package ephcli
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"io"
@@ -16,7 +15,7 @@ func (c *ClientEphemeralfiles) UploadEndpoint() string {
 	return fmt.Sprintf("%s/%s/upload", c.endpoint, apiVersion)
 }
 
-func (c *ClientEphemeralfiles) UploadWithProgressBar(fileToUpload string) error {
+func (c *ClientEphemeralfiles) Upload(fileToUpload string) error {
 	stat, err := os.Stat(fileToUpload)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -28,6 +27,7 @@ func (c *ClientEphemeralfiles) UploadWithProgressBar(fileToUpload string) error 
 	bar := progressbar.NewOptions64(stat.Size(), progressbar.OptionClearOnFinish(),
 		progressbar.OptionShowBytes(true), progressbar.OptionSetWidth(DefaultBarWidth),
 		progressbar.OptionSetDescription("uploadding file..."),
+		progressbar.OptionSetVisibility(!c.noProgressBar),
 	)
 
 	// Create a multipart form
@@ -74,66 +74,4 @@ func (c *ClientEphemeralfiles) UploadWithProgressBar(fileToUpload string) error 
 		return parseError(resp)
 	}
 	return nil
-}
-
-func (c *ClientEphemeralfiles) UploadWithoutProgressBar(fileToUpload string) error {
-	// get size of file
-	// stat, err := os.Stat(fileToUpload)
-	// if err != nil {
-	// 	if os.IsNotExist(err) {
-	// 		return fmt.Errorf("%w: %s", ErrFileNotFound, fileToUpload)
-	// 	}
-	// 	return err
-	// }
-	// Create a multipart form
-	bb := &bytes.Buffer{}
-	writer := multipart.NewWriter(bb)
-
-	defer writer.Close()
-
-	part, err := writer.CreateFormFile("uploadfile", fileToUpload)
-	if err != nil {
-		return fmt.Errorf("error creating form file: %w", err)
-	}
-	f, err := os.Open(fileToUpload)
-	if err != nil {
-		return fmt.Errorf("error opening file: %w", err)
-	}
-
-	defer f.Close()
-
-	if _, err := io.Copy(part, f); err != nil {
-		return fmt.Errorf("error copying file: %w", err)
-	}
-
-	f.Close()
-	writer.Close()
-
-	ctx := context.Background()
-	// Create a request with the form
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.UploadEndpoint(), bb)
-	if err != nil {
-		return fmt.Errorf("error creating request: %w", err)
-	}
-	// Add the token to the request
-	req.Header.Add("Authorization", "Bearer "+c.token)
-	req.Header.Add("Content-Type", writer.FormDataContentType())
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("error sending request: %w", err)
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return parseError(resp)
-	}
-	return nil
-}
-
-func (c *ClientEphemeralfiles) Upload(fileToUpload string) error {
-	if c.noProgressBar {
-		return c.UploadWithoutProgressBar(fileToUpload)
-	}
-	return c.UploadWithProgressBar(fileToUpload)
 }
